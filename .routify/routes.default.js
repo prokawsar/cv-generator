a47c0b7f1373e66e2b86f17ca7e5cf58702f70c9
Kawsar Ahmed <prokawsar@gmail.com>
2022-08-30 05:27:29 -0400

    init project with routify3

---
+++ .routify/routes.default.js
@@ -0,0 +1,56 @@
+
+
+export default {
+  "meta": {},
+  "id": "_default",
+  "_regex": {},
+  "_paramKeys": {},
+  "module": false,
+  "file": {
+    "path": "src/routes",
+    "dir": "src",
+    "base": "routes",
+    "ext": "",
+    "name": "routes"
+  },
+  "rootName": "default",
+  "routifyDir": import.meta.url,
+  "children": [
+    {
+      "meta": {},
+      "id": "_default_index_svelte",
+      "_regex": {},
+      "_paramKeys": {},
+      "name": "index",
+      "file": {
+        "path": "src/routes/index.svelte",
+        "dir": "src/routes",
+        "base": "index.svelte",
+        "ext": ".svelte",
+        "name": "index"
+      },
+      "asyncModule": () => import('../src/routes/index.svelte'),
+      "children": []
+    },
+    {
+      "meta": {
+        "dynamic": true,
+        "dynamicSpread": true
+      },
+      "_regex": {},
+      "_paramKeys": {},
+      "name": "[...404]",
+      "file": {
+        "path": ".routify/components/[...404].svelte",
+        "dir": ".routify/components",
+        "base": "[...404].svelte",
+        "ext": ".svelte",
+        "name": "[...404]"
+      },
+      "asyncModule": () => import('./components/[...404].svelte'),
+      "children": []
+    }
+  ]
+}
+
+import '@roxi/routify/lib/buildtime/plugins/devHelper/runtime.js'
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<div>
+	<h1>CV Generator</h1>
+
+	 <p>building work in progress</p>
+</div>
