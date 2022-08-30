6890550f31d3af62c44ee1bd7741ad7742810588
Kawsar Ahmed <prokawsar@gmail.com>
2022-08-30 07:16:31 -0400

    ui wip

--- src/routes/templates.svelte
+++ src/routes/templates.svelte
@@ -1,4 +1,29 @@
 <script>
 </script>
 
-<div>Template page</div>
+<div class="d-flex flex-row">
+  <div class="col-8">
+    <div class="mb-3">
+      <label for="" class="form-label">Designation</label>
+      <input type="text" class="form-control" id="" placeholder="Designation" />
+    </div>
+    <div class="mb-3">
+      <label for="" class="form-label">About you</label>
+      <textarea class="form-control" id="" rows="4" />
+    </div>
+
+    <div class="">
+      <button class="btn btn-success float-end">Have a look</button>
+    </div>
+  </div>
+  <div class="col-4">
+    <div
+      class="d-flex justify-content-center align-items-center p-2"
+      style="height: 300px;"
+    >
+      <span class="fs-4 text-muted text-uppercase"
+        >/* Reactive view of selected template will be here */</span
+      >
+    </div>
+  </div>
+</div>
