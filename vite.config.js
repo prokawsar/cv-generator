1765d7149915c4b1ae9c19abefe0a8c0ba903a45
Kawsar Ahmed <prokawsar@gmail.com>
2022-08-30 06:06:56 -0400

    added bootstrap

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import routify from '@roxi/routify/vite-plugin'
-import { defineConfig } from 'vite'
-import { mdsvex } from 'mdsvex'
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import routify from "@roxi/routify/vite-plugin";
+import { defineConfig } from "vite";
+import { mdsvex } from "mdsvex";
+import copy from "rollup-plugin-copy";
 
-const production = process.env.NODE_ENV === 'production'
+const production = process.env.NODE_ENV === "production";
 
 export default defineConfig({
     clearScreen: false,
@@ -17,10 +18,22 @@ export default defineConfig({
             compilerOptions: {
                 dev: !production,
             },
-            extensions: ['.md', '.svelte'],
-            preprocess: [mdsvex({ extension: 'md' })],
+            extensions: [".md", ".svelte"],
+            preprocess: [mdsvex({ extension: "md" })],
+        }),
+        copy({
+            targets: [
+                {
+                    src: "node_modules/bootstrap/dist/css/bootstrap.min.css",
+                    dest: "public/assets/bootstrap/css",
+                },
+                {
+                    src: "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+                    dest: "public/assets/bootstrap/js",
+                },
+            ],
+            hook: "writeBundle",
         }),
     ],
-
     server: { port: 1337 },
-})
+});
