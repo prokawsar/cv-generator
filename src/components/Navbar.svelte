ee9d5d52963be1fad9f819401391267c62763390
Kawsar Ahmed <prokawsar@gmail.com>
2022-08-30 06:32:32 -0400

    1st page

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">CV Generator</a>
+    <!-- <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div class="collapse navbar-collapse">
+      <input
+        class="form-control me-2"
+        type="search"
+        placeholder="Your name"
+        aria-label="Your name"
+      />
+      <button class="btn btn-outline-success" type="submit">Go</button>
+    </div> -->
+  </div>
+</nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,23 @@
 <script>
-
+  import Navbar from '../components/Navbar.svelte';
 </script>
 
-<div>
-	<h1>CV Generator</h1>
+<div class="container">
+  <Navbar />
 
-	 <p>building work in progress</p>
+  <div class="d-flex justify-content-between flex-row mt-2">
+    <div class="col-3" />
+    <div class="col-6">
+      <div class="d-flex flex-row">
+        <input
+          class="form-control form-control-lg me-2"
+          type="search"
+          placeholder="Your name"
+          aria-label="Your name"
+        />
+        <button class="btn btn-outline-success" type="submit">Go</button>
+      </div>
+    </div>
+    <div class="col-3" />
+  </div>
 </div>
