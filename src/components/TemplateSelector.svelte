fdff0a3f089dba23ced121ec092f43d2676afcbd
kahmed-at-979455188925 <prokawsar@gmail.com>
2022-08-31 17:10:45 +0600

    template selector

---
+++ src/components/TemplateSelector.svelte
@@ -0,0 +1,16 @@
+<div class="d-flex flex-row">
+  <div class="card template m-1">
+    <div class="card-body">Template one</div>
+  </div>
+  <div class="card template m-1">
+    <div class="card-body">Template two</div>
+  </div>
+</div>
+
+<style>
+  .template {
+    height: 200px;
+    width: 300px;
+    border-radius: 1rem;
+  }
+</style>
