f04607341c3ae348cb5f57240fde04c76818916e
Kawsar Ahmed <prokawsar@gmail.com>
2022-09-01 06:51:06 -0400

    remove comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,6 @@
 	};
 </script>
 
-<!-- <h1 class="text-3xl font-bold underline">Hello world!</h1> -->
-
 <div class="overflow-hidden shadow sm:rounded-md">
 	<div class="bg-gray-50 px-4 py-5 sm:p-6">
 		<div class="grid grid-cols-3">
@@ -32,11 +30,4 @@
 			</div>
 		</div>
 	</div>
-	<!-- <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
-		<button
-			type="submit"
-			class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-			>Save</button
-		>
-	</div> -->
 </div>
