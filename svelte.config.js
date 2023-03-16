83abcde46cd1a9a468f0c7a3dcdf88f20bbc72ba
Kawsar Ahmed <prokawsar@gmail.com>
2023-03-16 06:12:10 -0400

    js to ts

--- svelte.config.js
+++ svelte.config.js
@@ -15,15 +15,7 @@ const config = {
 			// if true, will deploy the app using edge functions
 			// (https://vercel.com/docs/concepts/functions/edge-functions)
 			// rather than serverless functions
-			edge: true,
-
-			// an array of dependencies that esbuild should treat
-			// as external when bundling functions
-			external: [],
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false
+			edge: true
 		})
 	}
 };
